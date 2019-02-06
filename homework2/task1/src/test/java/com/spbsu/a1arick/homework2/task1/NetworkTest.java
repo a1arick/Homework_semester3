76375a5bcc87442c33ee298f9742211108ba4fdf
a1arick <31973557+a1arick@users.noreply.github.com>
2019-02-06 20:46:39 +0300

    add new Test

--- homework2/task1/src/test/java/com/spbsu/a1arick/homework2/task1/NetworkTest.java
+++ homework2/task1/src/test/java/com/spbsu/a1arick/homework2/task1/NetworkTest.java
@@ -107,4 +107,45 @@ public class NetworkTest {
         assertEquals(1, network.getHealthySize());
         assertEquals(3, network.getInfectSize());
     }
+    
+    @Test
+    public void infect5() {
+        Network network = new Network();
+        Computer a = new Computer(1, OperationSystem.WINDOWS);
+        Computer b = new Computer(2, OperationSystem.LINUX);
+        Computer c = new Computer(3, OperationSystem.IOS);
+        Computer d = new Computer(4, OperationSystem.ANDROID);
+        network.add(a);
+        assertEquals(1, network.size());
+        network.add(b);
+        assertEquals(2, network.size());
+        network.add(c);
+        assertEquals(3, network.size());
+        network.add(d);
+        assertEquals(4, network.size());
+        network.connect(a.getId(), b.getId());
+        network.connect(b.getId(), c.getId());
+        assertEquals(1, a.getConnectedComputers().size());
+        assertEquals(2, b.getConnectedComputers().size());
+        assertEquals(1, c.getConnectedComputers().size());
+        network.connect(a.getId(), c.getId());
+        assertEquals(2, a.getConnectedComputers().size());
+        assertEquals(2, c.getConnectedComputers().size());
+        network.connect(a.getId(), d.getId());
+        assertEquals(3, a.getConnectedComputers().size());
+        assertEquals(1, d.getConnectedComputers().size());
+        network.connect(b.getId(), d.getId());
+        assertEquals(3, b.getConnectedComputers().size());
+        assertEquals(2, d.getConnectedComputers().size());
+        network.connect(c.getId(), d.getId());
+        assertEquals(3, c.getConnectedComputers().size());
+        assertEquals(3, d.getConnectedComputers().size());
+        network.infect(1, new Virus("Trojan", 0.6));
+        assertEquals(1, network.getHealthySize());
+        assertEquals(3, network.getInfectSize());
+        assertTrue(a.isInfected());
+        assertFalse(b.isInfected());
+        assertTrue(c.isInfected());
+        assertTrue(d.isInfected());
+    }
 }
