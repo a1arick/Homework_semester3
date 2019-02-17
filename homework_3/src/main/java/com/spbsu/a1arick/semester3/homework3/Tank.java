1f2378f5b7e497becdc896da5e034dd30e02efdc
a1arick <31973557+a1arick@users.noreply.github.com>
2019-02-18 01:20:06 +0300

    add javadoc

--- homework_3/src/main/java/com/spbsu/a1arick/semester3/homework3/Tank.java
+++ homework_3/src/main/java/com/spbsu/a1arick/semester3/homework3/Tank.java
@@ -2,10 +2,12 @@ package com.spbsu.a1arick.semester3.homework3;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * Interface for bots and player tanks
+ */
 public interface Tank {
     void update(double time);
     void draw(GraphicsContext gc);
-    void setWorld(World world);
     void checkLife(Shot shot);
     boolean isDead();
 }
