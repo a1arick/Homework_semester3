eb9a97dfce005fd0a3470f9caf0d4c036cf19051
a1arick <31973557+a1arick@users.noreply.github.com>
2019-02-18 01:19:39 +0300

    Update Shot.java

--- homework_3/src/main/java/com/spbsu/a1arick/semester3/homework3/Shot.java
+++ homework_3/src/main/java/com/spbsu/a1arick/semester3/homework3/Shot.java
@@ -4,53 +4,75 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Shot {
+    private World world;
     private double x;
     private double dx;
     private double y;
     private double dy;
-    private double radius = 20;
+    private double radius = 15;
 
-    private World world;
 
-    public Shot(double x, double y, double dx, double dy) {
+    public Shot(double x, double y, double dx, double dy, World world) {
         this.x = x;
         this.dx = dx;
         this.y = y;
         this.dy = dy;
+        this.world = world;
     }
 
+    /**
+     * Draws shot
+     *
+     * @param graphicsContext graphicsContext where shot will be drawn
+     */
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.fillOval(x, y, 10, 10);
+    }
+
+    /**
+     * Updates position shot
+     *
+     * @param time time last update
+     */
     public void update(double time) {
         x += dx * time;
         y += dy * time;
         dy += world.getG() * time;
     }
 
-    public void draw(GraphicsContext graphicsContext) {
-        graphicsContext.setFill(Color.RED);
-        graphicsContext.fillOval(x, y, 10, 10);
-    }
-
-    public void setWorld(World world) {
-        this.world = world;
-    }
-
     /**
      * Checks is it exploded according position in the world
+     *
      * @return true if it is and false otherwise
      */
     public boolean isExploded() {
         return y > world.getY(x);
     }
 
-
+    /**
+     * x coordinate shot
+     *
+     * @return position x coordinate shot
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * y coordinate shot
+     *
+     * @return position y coordinate shot
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * radius shot
+     *
+     * @return radius shot
+     */
     public double getRadius() {
         return radius;
     }
